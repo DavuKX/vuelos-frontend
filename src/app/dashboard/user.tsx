640c0ad308b8b931918c29bbb08797c1243e49ad
DavuKX <duvandavid2001@gmail.com>
2024-11-17 11:53:52 -0500

    feat: set up main template for the application

---
+++ src/app/dashboard/user.tsx
@@ -0,0 +1,57 @@
+import {Button} from '@/components/ui/button';
+import Image from 'next/image';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import Link from 'next/link';
+
+export async function User() {
+    let user = null;
+
+    return (
+        <div className="relative ml-auto flex-1 md:grow-0">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="outline"
+                        size="icon"
+                        className="overflow-hidden rounded-full"
+                    >
+                        <Image
+                            src={'/placeholder-user.jpg'}
+                            width={36}
+                            height={36}
+                            alt="Avatar"
+                            className="overflow-hidden rounded-full"
+                        />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator/>
+                    <DropdownMenuSeparator/>
+                    {user ? (
+                        <DropdownMenuItem>
+                            <form
+                                action={async () => {
+                                    'use server';
+                                }}
+                            >
+                                <button type="submit">Sign Out</button>
+                            </form>
+                        </DropdownMenuItem>
+                    ) : (
+                        <DropdownMenuItem>
+                            <Link href="/login">Sign In</Link>
+                        </DropdownMenuItem>
+                    )}
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    );
+}
