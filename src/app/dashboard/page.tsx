05d972bb23988471d11a516f66cb6889ac188352
DavuKX <duvandavid2001@gmail.com>
2024-11-23 12:57:22 -0500

    minor changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,34 @@
+'use client';
 import SearchFlights from "@/app/dashboard/search-flights";
+export interface Filter {
+    origin: string;
+    destiny: string;
+    date: string;
+    passengers: number;
+}
 
-export default async function HomePage() {
+export default function HomePage() {
+    const filters: Filter = {
+        origin: '',
+        destiny: '',
+        date: '',
+        passengers: 1
+    };
+
+    const onDataChange = (filter: Filter) => {
+        filters.origin = filter.origin;
+        filters.destiny = filter.destiny;
+        filters.date = filter.date;
+        filters.passengers = filter.passengers;
+    }
+
+    const onSearch = async () => {
+        console.log(filters);
+    }
+
     return (
         <>
-            <SearchFlights />
+            <SearchFlights onDataChange={onDataChange} onSearch={onSearch} />
         </>
     );
 }
--- src/app/dashboard/search-flights.tsx
+++ src/app/dashboard/search-flights.tsx
@@ -1,41 +1,82 @@
+'use client';
+
 import React from 'react';
 import {Button, Card, FormControl, TextField} from "@mui/material";
 import {DatePicker} from "@mui/x-date-pickers";
 import {Search} from "lucide-react";
+import {Filter} from "@/app/dashboard/page";
 
-const SearchFlights = () => {
+interface SearchFlightsProps {
+    onDataChange: (filter: Filter) => void;
+    onSearch: () => void;
+}
+
+const SearchFlights = ({ onDataChange, onSearch }: SearchFlightsProps) => {
+    const [filter, setFilter] = React.useState({
+        origin: '',
+        destiny: '',
+        date: '',
+        passengers: 1
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilter({
+            ...filter,
+            [event.target.id]: event.target.value
+        });
+
+        onDataChange(filter);
+    };
+
     return (
         <Card className="p-4">
             <h3 className="text-2xl font-semibold leading-none tracking-tight mb-2">
                 Vuelos
             </h3>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                <FormControl className="col-span-2">
+            <div className="grid grid-cols-2 gap-4">
+                <FormControl className="col-span-1">
                     <TextField
                         id="origin"
                         label="Origen"
                         variant="outlined"
                         size="small"
+                        onChange={handleChange}
+                        value={filter.origin}
                     />
+                </FormControl>
+                <FormControl className="col-span-1">
                     <TextField
                         id="destiny"
                         label="Destino"
                         variant="outlined"
                         size="small"
+                        onChange={handleChange}
+                        value={filter.destiny}
                     />
                 </FormControl>
-                <FormControl className="col-span-2">
-                    <DatePicker label="Ida" slotProps={{ textField: { size: 'small'}}} />
+                <FormControl className="col-span-1">
+                    <DatePicker
+                        label="Fecha"
+                        slotProps={{ textField: { size: 'small'}}}
+                    />
+                </FormControl>
+                <FormControl className="col-span-1">
                     <TextField
                         id="passengers"
                         label="Pasajeros"
                         variant="outlined"
                         size="small"
                         type="number"
+                        onChange={handleChange}
+                        value={filter.passengers}
                     />
                 </FormControl>
-                <FormControl className="col-span-4">
-                    <Button variant="outlined" color="secondary" startIcon={<Search/>}>
+                <FormControl className="col-span-2">
+                    <Button
+                        variant="outlined"
+                        color="secondary"
+                        startIcon={<Search/>} onClick={onSearch}
+                    >
                         Buscar
                     </Button>
                 </FormControl>
