e583ee274b809a8dd1a99ddc5311c56fcc93b272
DavuKX <duvandavid2001@gmail.com>
2024-11-25 23:54:23 -0500

    Add toast notification for expired session in axios interceptor

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,6 +1,7 @@
 import { getCookie } from "@/lib/cookieUtils";
 import axios from "axios";
 import { redirect } from "next/navigation";
+import { toast } from "react-toastify";
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:8080/api/v1/",
@@ -27,6 +28,7 @@ axiosInstance.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response && error.response.status === 401) {
+            toast.error("Sesion expirada, por favor inicie sesion nuevamente");
             redirect("/auth/login");
         }
         return Promise.reject(error);
